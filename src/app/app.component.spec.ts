f4ce49380af6deae87cc636ba2549821335e5759
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-01 13:54:01 -0300

    add test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
+@Component({selector: 'app-header', template: ''})
+class HeaderStubComponent {}
 
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent { }
+
+@Component({selector: 'app-footer', template: ''})
+class FooterStubComponent {}
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        SharedModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderStubComponent,
+        RouterOutletStubComponent,
+        FooterStubComponent
       ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     }).compileComponents();
   }));
 
@@ -18,18 +32,34 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }); 
 
-  it(`should have as title 'dev-jobs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dev-jobs');
-  });
-
-  it('should render title', () => {
+  it('should it have a element app-header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dev-jobs app is running!');
+    expect(compiled.querySelector('app-header')).toBeTruthy();
   });
+
+  it('should it have a element main', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('main')).toBeTruthy();
+  });
+
+  it('should it have a element main a router outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled: HTMLElement = fixture.nativeElement;
+    expect(compiled.querySelector('main router-outlet')).toBeTruthy();
+  });
+
+  it('should it have a element app-footer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-footer')).toBeTruthy();
+  });
+
 });
