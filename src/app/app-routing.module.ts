730d47a125a539052f0fd1c0cdfd871d1b792ea0
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-01 04:45:53 -0300

    fix home route and prefix add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule) }
+  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), pathMatch: 'full'}
 ];
 
 @NgModule({
