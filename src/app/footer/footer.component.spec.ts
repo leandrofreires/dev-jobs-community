c16560e861cb22d92f3e6c96b96b5b7d7b3b0776
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-01 13:52:37 -0300

    customize footer

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
 import { FooterComponent } from './footer.component';
-
+import { HarnessLoader } from '@angular/cdk/testing';
+import {MatButtonHarness} from '@angular/material/button/testing';
+import { SharedModule } from '../shared/shared.module';
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
-
+  let loader: HarnessLoader;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ FooterComponent ],
+      imports: [
+        SharedModule
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should it have a footer tag', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('footer')).toBeTruthy();
+  });
+
+  it('should it have a nav tag inner footer', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('footer nav')).toBeTruthy();
+  });
+
+  it('should it have two buttons on nav inner footer', async () => {
+    const footerLoader = await loader.getChildLoader('footer nav');
+    const buttons = await footerLoader.getAllHarnesses(MatButtonHarness);
+    expect(buttons.length).toEqual(2);
   });
 });
