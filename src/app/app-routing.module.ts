a6ad18ac73a0c3b44a1aa08694e450166e1d73aa
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-01 04:13:55 -0300

    create home page as module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
