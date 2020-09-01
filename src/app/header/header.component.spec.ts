6050ee03854794e349509193ee3ceafbad4cc84e
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-01 13:53:41 -0300

    removed row from header tolbar

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,25 +1,63 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatToolbarHarness } from '@angular/material/toolbar/testing';
+import { SharedModule } from '../shared/shared.module';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-
+  let loader: HarnessLoader;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [ SharedModule ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should it have a header tag', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('header')).toBeTruthy();
+  });
+
+  it('should it have a mat-tolbar inner header', async () => {
+    const headerLoader = await loader.getChildLoader('header');
+    const toolbar = await headerLoader.getHarness(MatToolbarHarness);
+    expect(toolbar).toBeTruthy();
+  });
+
+  it('should it have a h1 tag on toolbar', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('header mat-toolbar h1')).toBeTruthy();
+  });
+
+  it('should it have h1 title as Dev Jobs Community', () => {
+    const compiled: HTMLElement = fixture.nativeElement;
+    expect(compiled.querySelector('header mat-toolbar h1').textContent).toEqual('Dev Jobs Community');
+  });
+
+  it('should it have a nav tag', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('header mat-toolbar nav')).toBeTruthy();
+  });
+
+  it('should it have one link for repo inner nav', () => {
+    const compiled: HTMLElement = fixture.nativeElement;
+    expect(compiled.querySelectorAll('nav a').length).toEqual(1);
+    expect(compiled.querySelector('nav a').getAttribute('title')).toEqual('project on github');
   });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -22,4 +22,36 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should it have article tag', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('article')).toBeTruthy();
+  });
+
+  it('should it have one logo inner article', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('article img#logo-project')).toBeTruthy();
+  });
+
+  it('should it have one header on article', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('article header')).toBeTruthy();
+  });
+
+  it('should it have one title and subtitle on header', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('article header h1')).toBeTruthy();
+    expect(compiled.querySelector('article header h2')).toBeTruthy();
+  });
+
+  it('the text on title should to be', () => {
+    const defaultTitle = "Dev Jobs Community is a helper for devs looking for opportunities";
+    const compiled: HTMLElement = fixture.nativeElement;
+    expect(compiled.querySelector('article header h1').textContent).toEqual(defaultTitle)
+  });
+  it('the text on subtitle should to be', () => {
+    const defaultSubTitle = "This project is to bring together the github repositories with offers of vacancies available in different areas";
+    const compiled: HTMLElement = fixture.nativeElement;
+    expect(compiled.querySelector('article header h2').textContent).toEqual(defaultSubTitle)
+  });
 });
