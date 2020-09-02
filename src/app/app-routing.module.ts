76aa76416436bc31484601790f2711bdea1c1048
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-02 13:01:57 -0300

    create jobs module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), pathMatch: 'full'}
+  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), pathMatch: 'full'},
+  { path: 'jobs', loadChildren: () => import('./jobs/jobs.module').then(mod => mod.JobsModule), pathMatch: 'full'},
 ];
 
 @NgModule({
---
+++ src/app/jobs/jobs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JobsRoutingModule } from './jobs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    JobsRoutingModule
+  ]
+})
+export class JobsModule { }
