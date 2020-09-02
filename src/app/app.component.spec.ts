05ed602d3890ab6098fbac6f711f888daf94b6d4
Leandro Freire <leandro.freire.work@gmail.com>
2020-09-02 14:48:54 -0300

    pass broweser module and fix sidenav

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations/';
 @Component({selector: 'app-header', template: ''})
 class HeaderStubComponent {}
 
@@ -11,10 +14,15 @@ class RouterOutletStubComponent { }
 
 @Component({selector: 'app-footer', template: ''})
 class FooterStubComponent {}
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let loader: HarnessLoader;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [
+        BrowserAnimationsModule,
         RouterTestingModule,
         SharedModule
       ],
@@ -22,21 +30,27 @@ describe('AppComponent', () => {
         AppComponent,
         HeaderStubComponent,
         RouterOutletStubComponent,
-        FooterStubComponent
+        FooterStubComponent,
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     }).compileComponents();
-  }));
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component  = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  }); 
+    expect(component).toBeDefined();
+  });
+
+  it('should it have a sidenav container component', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('mat-sidenav-container')).toBeTruthy();
+  });
 
   it('should it have a element app-header', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('app-header')).toBeTruthy();
   });
@@ -48,7 +62,7 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('main')).toBeTruthy();
   });
 
-  it('should it have a element main a router outlet', () => {
+  it('should it have a element main in router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled: HTMLElement = fixture.nativeElement;
