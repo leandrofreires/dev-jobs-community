7a0457d975544325f091bd1caf878bbf5e97ebc9
Leandro freire <leandro.freire.work@gmail.com>
2020-09-25 05:37:00 -0300

    filter skills split on comma

--- src/app/jobs/search.pipe.ts
+++ src/app/jobs/search.pipe.ts
@@ -2,21 +2,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Issue } from './issue';
 
 @Pipe({
-  name: 'search'
+  name: 'search',
 })
 export class SearchPipe implements PipeTransform {
-
-  transform(issues:  Issue[], searchText: string): Issue[] {
-    if(!issues){
+  transform(issues: Issue[], searchText: string): Issue[] {
+    if (!issues) {
       return [];
     }
-    if(!searchText){
+    if (!searchText) {
       return issues;
     }
-    let regex = new RegExp(searchText, 'ig');
-    return issues.filter( issue => {
-      return issue.title.match(regex) || issue.body.match(regex)
-    })
+    let skills = searchText.split(',').map((s) => s.trim());
+    let filtered: Issue[] = [];
+    skills.forEach((skill) => {
+      let regex = new RegExp(skill, 'ig');
+      filtered = filtered.concat(issues.filter((issue) => {
+        return issue.title.match(regex) || issue.body.match(regex);
+      }));
+    });
+    return filtered;
   }
-
 }
