3ded24598cd863a8313e9ca1df48e63068fcc4b8
Leandro freire <leandro.freire.work@gmail.com>
2020-09-24 22:48:41 -0300

    increment search pipe

--- src/app/jobs/jobs-list/jobs-list.component.ts
+++ src/app/jobs/jobs-list/jobs-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, SecurityContext } from '@angular/core';
+import { Component, Input, OnInit, SecurityContext } from '@angular/core';
 import { JobsService, Repository } from '../jobs.service';
 import { Issue } from '../issue';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-jobs-list',
@@ -10,6 +9,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class JobsListComponent implements OnInit {
 
+  @Input() search: string;
+
   constructor(private jobsService: JobsService ) { }
 
   issues: Issue[] = [];
@@ -17,12 +18,23 @@ export class JobsListComponent implements OnInit {
   repositories: Repository[] = this.jobsService.repos;
 
   ngOnInit(): void {
-    this.repositories.forEach(async (repo) => {
-
+    this.loadData();
+  }
+  async loadData() {
+    const promises = this.repositories.map(async (repo, idx) => {
       let issueResponse = await this.jobsService.getIssues(repo).toPromise();
-
       this.issues = this.issues.concat(issueResponse);
-      
+    });
+    await Promise.all(promises);
+    this.sort()
+    this.jobsService.loadDone.emit(true)
+  }
+
+  sort() {
+    this.issues.sort(function(a,b){
+      var keyA = new Date(a.created_at), keyB = new Date(b.created_at);
+      if(keyA > keyB) return -1;
+      if(keyA < keyB) return 1;
     });
   }
 }
