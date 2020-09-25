986fd63798b3b7e0eb13f96608813349be09ea6a
Leandro freire <leandro.freire.work@gmail.com>
2020-09-24 22:49:25 -0300

    router reorder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), pathMatch: 'full'},
   { path: 'jobs', loadChildren: () => import('./jobs/jobs.module').then(mod => mod.JobsModule), pathMatch: 'full'},
   { path: 'about', loadChildren: () => import('./about/about.module').then(mod => mod.AboutModule), pathMatch: 'full'},
+  { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
