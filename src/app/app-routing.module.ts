56f7d61dd75d1e45ebee95bf62d892645c1e4872
Leandro freire <leandro.freire.work@gmail.com>
2020-09-24 22:48:19 -0300

    import page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule), pathMatch: 'full'},
